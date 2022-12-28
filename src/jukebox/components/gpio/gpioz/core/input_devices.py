b895ebe90a660994a800f9308ab78dd452a8ecfe
wunderlich.marc@googlemail.com <wunderlich.marc@web.de>
2022-12-28 08:33:25 +0100

    Scaling and Smoothing

--- src/jukebox/components/gpio/gpioz/core/input_devices.py
+++ src/jukebox/components/gpio/gpioz/core/input_devices.py
@@ -759,7 +759,7 @@ class AnalogInput(NameMixin):
             lower_bound = self._last_value - self._step
             upper_bound = self._last_value + self._step
             if current_value_to_scale < lower_bound or current_value_to_scale > upper_bound:
-                current_value = current_value // self._step * self._step
+                current_value = current_value_to_scale // self._step * self._step
                 logger.debug(f"{self.name} - New value: {current_value}")
                 self._last_value = current_value
                 pub_value = self._last_value if not self._invert else self._max_value - self._last_value
